8454d3f918ecd10204c89c81228ef5feda4d2efe
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 12:13:10 +0700

    W08P02

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,17 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            int[] data = new int[10];
+            data[-1] = 0;
+            data[1] = 1;
+            data[9] = 9;
+            data[10] = 10;
+            Console.WriteLine(data[-1]);
+            Console.WriteLine(data[1]);
+            Console.WriteLine(data[9]);
+            Console.WriteLine(data[10]);
+            Console.ReadLine();
+
         }
     }
 }
