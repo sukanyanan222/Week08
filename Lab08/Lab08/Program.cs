b6010bce1bec26733b5aa8e773b790bf42ec21ac
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:59:50 +0700

    W08P22

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Lab08
+namespace ArrayListDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            ArrayList list1 = new ArrayList();
+            list1.Add(10);
+            list1.Add(20);
+            list1.Add(30);
+            list1.Add(40);
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine(list1[i]);
+            }
+            ArrayList list2 = new ArrayList(list1);
+            list2[2] = 55;
+            list2[3] = 66;
+            list2[4] = 77;
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                Console.WriteLine(list2[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
-
-    
