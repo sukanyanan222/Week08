a05af665103ff030ed79dc50dcdb36677ed508ca
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 13:23:37 +0700

    W08P12

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,9 +10,40 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            string[] Names = new string[]
+    {
+       "Afghanistan",
+       "Albania",
+       "Algeria",
+       "American Samoa",
+       "Andorra",
+          
+       "Virgin Islands, British",
+       "Virgin Islands, US",
+       "Wallis and Futuna",
+       "Western Sahara",
+       "Yemen",
+       "Zambia",
+       "Zimbabwe",
+    };
+            // C# program that uses LINQ
+            var sortAscending = from Country in Names
+                                orderby Country
+                                select Country;
+            var sortDescending = from Country in Names
+                                 orderby Country descending
+                                 select Country;
+            Console.WriteLine("------String sort ascending----------");
+            foreach (string c in sortAscending)
+                Console.WriteLine(c);
+            Console.WriteLine("------String sort descending----------");
+            foreach (string c in sortDescending)
+                Console.WriteLine(c);
+            Console.ReadKey();
         }
+
     }
 }
+
 
     
