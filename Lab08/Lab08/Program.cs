583c73b1327f9c31927d2a850601ab1cfdd81565
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:20:23 +0700

    W08P25

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("RED");
+            queue1.Enqueue("BLUE");
+            queue1.Enqueue("YELLOW");
+            queue1.Enqueue("GREEN");
+
+            Console.WriteLine("The elements in the queue are:" + queue1.Count);
+            queue1.Clear();
+            Console.WriteLine("The elements in the queue are after the clear method: " + queue1.Count);
+            Console.ReadLine();
+
         }
     }
 }
