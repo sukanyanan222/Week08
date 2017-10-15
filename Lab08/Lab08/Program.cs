cdcdfc7b90b440bf1fadf306327fb9ca6a701b0b
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:27:58 +0700

    W08P28

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace StackDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Stack<string> stack1 = new Stack<string>();
+            stack1.Push("************");
+            stack1.Push("RED");
+            stack1.Push("GREEN");
+            stack1.Push("BLUE");
+            stack1.Push("YELLOW");
+            stack1.Push("***********");
+            stack1.Push("** COLOR **");
+            stack1.Push("***********");
+            Console.WriteLine("The elements in the stack1 are as:");
+            foreach (string s in stack1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("The element RED contain in the stack "
++ stack1.Contains("RED"));
+            Console.WriteLine("The element YELLOW contain in the stack "
++ stack1.Contains("YELLOW"));
+            Console.WriteLine("The element BLACK contain in the stack "
++ stack1.Contains("BLACK"));
+            Console.ReadLine();
+
         }
     }
 }
