eb08725e9299709e975c6249c085134b065324e4
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:23:10 +0700

    W08P19

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,9 +10,17 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            string[][] jagged = new string[2][];
+            jagged[0] = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            jagged[1] = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            Console.WriteLine("Day_names of week : " + (string.Join(",", jagged[0])));
+            Console.WriteLine("Month_names of year : " + (string.Join(",", jagged[1])));
+            Console.ReadKey();
         }
+
     }
 }
+
 
     
