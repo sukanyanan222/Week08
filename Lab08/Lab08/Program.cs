276595fc58359250a4e6a8332e32fbd9747d9cba
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:54:25 +0700

    W08P37

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Lab08
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "Sunday");
+            weeks.Add("2", "Monday");
+            weeks.Add("3", "Tuesday");
+            weeks.Add("4", "Wednesday");
+            weeks.Add("5", "Thursday");
+            weeks.Add("6", "Friday");
+            weeks.Add("7", "Saturday");
+
+            Console.WriteLine("---- elements in Hashtable weeks ----");
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.WriteLine("\n---- weeks.Remove(\"4\"); ----");
+            weeks.Remove("4");
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.WriteLine("\n---- weeks.Clear(); ----");
+            weeks.Clear();
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
