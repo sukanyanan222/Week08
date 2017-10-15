560887a00a40b3c146589f86db17ed0d5e7b3b3d
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 13:12:18 +0700

    W08P10

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,9 +10,49 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            // Array of characters.
+            char[] array1 = { 'q', 'w', 'e', 'r', 't',
+                'y', 'u', 'i', 'o', 'p' };
+            Array.Sort(array1);
+
+            // Array of strings.
+            string[] colors = new string[] { "red",
+              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
+            Array.Sort(colors);
+
+            string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
+              "Indonesia", "Singapore", "Thailand", "Philippines",
+              "Vietnam", "Brunei Darussalam", "Laos", "Myanmar" };
+
+            // C# program that uses LINQ
+            var sortAscending = from country in AseanCountries
+                                orderby country
+                                select country;
+            var sortDescending = from country in AseanCountries
+                                 orderby country descending
+                                 select country;
+
+            // print output
+            Console.WriteLine("------Character sorting----------");
+            foreach (var c in array1)
+                Console.WriteLine(c);
+
+            Console.WriteLine("------String sorting----------");
+            foreach (string color in colors)
+                Console.WriteLine(color);
+
+            Console.WriteLine("------String sort ascending----------");
+            foreach (string c in sortAscending)
+                Console.WriteLine(c);
+
+            Console.WriteLine("------String sort descending----------");
+            foreach (string c in sortDescending)
+                Console.WriteLine(c);
+            // wait
+            Console.ReadLine();
         }
     }
 }
 
+      
     
