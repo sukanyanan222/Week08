a21359aebd834aaeff92c6037c6b187dda111071
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 12:22:03 +0700

    W08P04

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,14 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
+            Console.WriteLine("Length of data = {0}", data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            Console.ReadLine();
+
         }
     }
 }
