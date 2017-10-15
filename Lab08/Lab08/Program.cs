b49282a795abd8b245a66663ff0e6f7072143b2a
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:53:52 +0700

    W08P020

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace ArrayListDemo
 {
@@ -10,7 +7,7 @@ namespace ArrayListDemo
     {
         static void Main(string[] args)
         {
-          ArrayList = new ArrayList();
+            ArrayList list = new ArrayList();
             Console.WriteLine("Create a list");
             Console.WriteLine("List capacity = {0,2}", list.Capacity);
             for (int i = 0; i < 100; i++)
@@ -18,7 +15,7 @@ namespace ArrayListDemo
                 Console.Write("Add int to list : {0,2} => ", i);
                 list.Add(i);
                 Console.WriteLine("List count =  {0,3}, capacity = {1,3}  ",
-    list.Count, list.Capacity );
+    list.Count, list.Capacity);
             }
             Console.WriteLine("-----------------------");
             for (int i = 0; i < 100; i++)
