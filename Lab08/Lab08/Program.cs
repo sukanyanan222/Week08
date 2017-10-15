b210161de0c3acd34b0e66f2ed066c8fccec7d31
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:17:41 +0700

    W08P17

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,8 +10,25 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            int[,] multiplyTable = new int[12, 12];
+            for (int i = 0, table = 2; i < multiplyTable.GetLength(0); i++, table++)
+            {
+                for (int j = 0; j < multiplyTable.GetLength(1); j++)
+                {
+                    multiplyTable[j, i] = table * (j + 1);
+                }
+            }
+            for (int row = 0; row < multiplyTable.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                {
+                    Console.Write("{0,5}", multiplyTable[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
+
     }
 }
 
