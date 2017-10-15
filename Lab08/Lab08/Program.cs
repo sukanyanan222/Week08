e8d1ee9f8ba57da0f5f1b12efa885a3eae8644a9
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:10:26 +0700

    W08P23

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Queue<int> q = new Queue<int>();
+            Console.WriteLine("-- enqueue 3 items -- ");
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- enqueue more items -- ");
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- dequeue -- ");
+            int i = q.Dequeue();
+            Console.WriteLine("i = {0}", i);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+            Console.ReadLine();
+
         }
     }
 }
