0b93382e465759905f8562cf1af1307e1613552b
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:51:51 +0700

    W08P36

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Lab08
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "Sunday");
+            weeks.Add("2", "Monday");
+            weeks.Add("3", "Tuesday");
+            weeks.Add("4", "Wednesday");
+            weeks.Add("5", "Thursday");
+            weeks.Add("6", "Friday");
+            weeks.Add("7", "Saturday");
+
+            // Method ContainKey()
+            Console.WriteLine("The  key element \"8\" is contain in the Hashtable weeks: " + weeks.ContainsKey("8"));
+
+            // Method ContainValue()
+            Console.WriteLine("The  key element \"Wednesday\" is contain in theHashtable weeks: " + weeks.ContainsValue("Wednesday"));
+
+            Console.ReadLine();
         }
+
     }
 }
+
 
     
