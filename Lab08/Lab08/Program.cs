b6a4adee0893f719596e2f6d85ef18977b60b55b
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 18:41:43 +0700

    W08P15

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,43 +10,7 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-            //find "land"
-            string[] ArrayA = Array.FindAll(Names, element => element.EndsWith("land",
-                      StringComparison.Ordinal));
-            string[] ArrayB;
-            //find "stan"
-            string[] ArrayC = Array.FindAll(Names, element => element.EndsWith("stan",
-                      StringComparison.Ordinal));
-            string[] ArrayD;
-            // copy by operator =
-            ArrayB = ArrayA;
-            ArrayD = ArrayC;
-
-            Console.WriteLine("*** Array copy by operator = ***");
-            Console.WriteLine("===== Countrys =====");
-            for (int i = 0; i < Names.Length; i++)
-            { Console.WriteLine("Country[{0}] = {1}", i + 1, Names[i]); }
-            Console.WriteLine("--------------------");
-            Console.WriteLine("Find 'land' : " + (string.Join(", ", ArrayB)));
-            Console.WriteLine("Find 'stan' : " + (string.Join(", ", ArrayD)));
-            Console.ReadKey();
-        }
-        public static string[] Names = new string[]
-        {
-    "Afghanistan",
-    "Albania",
-    "Algeria",
-    "American Samoa",
-    "Andorra",
-       
-    "Virgin Islands, British",
-    "Virgin Islands, US",
-    "Wallis and Futuna",
-    "Western Sahara",
-    "Yemen",
-    "Zambia",
-    "Zimbabwe",
-        };
+          
     }
 
 }
