d64ebd343d0f61a46474c06278a5431f06b10b7c
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:23:17 +0700

    W08P26

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(10);
+            queue1.Enqueue(20);
+            queue1.Enqueue(30);
+            queue1.Enqueue(40);
+            Console.WriteLine("The queue elements are:");
+            foreach (int i in queue1)
+            {
+                Console.WriteLine(i);
+            }
+            ArrayList array = new ArrayList(queue1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
         }
     }
 }
