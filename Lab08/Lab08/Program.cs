f4bc3f6d374aadc712e742b5f6cb3ebc4f9030dc
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:30:36 +0700

    W08P29

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace StackDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Stack<string> stack1 = new Stack<string>();
+            stack1.Push("************");
+            stack1.Push("RED");
+            stack1.Push("GREEN");
+            stack1.Push("BLUE");
+            stack1.Push("YELLOW");
+            stack1.Push("***********");
+            stack1.Push("** COLOR **");
+            stack1.Push("***********");
+            Console.WriteLine("The elements in the stack1 are as:");
+            foreach (string s in stack1)
+            {
+                Console.WriteLine(s);
+            }
+            stack1.Clear();
+            Console.WriteLine("After apply the clear method the elements in thestack are: " + stack1.Count);
+            Console.ReadLine();
+
         }
     }
 }
