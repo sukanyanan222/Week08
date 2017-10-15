e526678d8db13b1ab7d3345a29b0bb15f709a0f9
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:32:52 +0700

    W08P30

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(10);
+            stack1.Push(20);
+            stack1.Push(30);
+            stack1.Push(40);
+            Console.WriteLine("The stack elements are:");
+            foreach (int i in stack1)
+            {
+                Console.WriteLine(i);
+            }
+            ArrayList array = new ArrayList(stack1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
         }
     }
 }
