5932c242a1f2feaec2b4605fcd56b3410ab47ad2
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 13:14:44 +0700

    W08P11

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,23 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            string[] monthsName = new string[]
+     { "January","February","March","April","May","June","July","August", "September", "October", "November",  "December" };
+            // C# program that uses LINQ
+            var sortAscending = from months in monthsName
+                                orderby months
+                                select months;
+            var sortDescending = from months in monthsName
+                                 orderby months descending
+                                 select months;
+            Console.WriteLine("------String sort ascending----------");
+            foreach (string c in sortAscending)
+                Console.WriteLine(c);
+            Console.WriteLine("------String sort descending----------");
+            foreach (string c in sortDescending)
+                Console.WriteLine(c);
+            Console.ReadKey();
+
         }
     }
 }
