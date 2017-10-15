f0060f6029c8a9e0e355703a15e0d717e85a2e8b
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:25:22 +0700

    W08P27

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace StackDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Stack<int> s = new Stack<int>();
+            Console.WriteLine("-- add 4 items -- ");
+            s.Push(10);
+            s.Push(20);
+            s.Push(30);
+            s.Push(40);
+            Console.Write("Stack contains : ");
+            foreach (var item in s)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "s.Count  = {0}", s.Count);
+            Console.WriteLine("s.Peek = {0}", s.Peek());
+
+            Console.WriteLine("-- add more items -- ");
+            s.Push(50);
+            s.Push(60);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in s)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "s.Count  = {0}", s.Count);
+            Console.WriteLine("s.Peek = {0}", s.Peek());
+
+            Console.WriteLine("-- push -- ");
+            int i = s.Pop();
+            Console.WriteLine("i = {0}", i);
+
+            Console.Write("Stack contains : ");
+            foreach (var item in s)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "s.Count  = {0}", s.Count);
+            Console.WriteLine("s.Peek = {0}", s.Peek());
+            Console.ReadLine();
+
+
         }
     }
 }
