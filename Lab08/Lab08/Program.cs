ac129c651811f3a757175a2ae22cbce022e6ed1f
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:05:06 +0700

    W08P15

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab08
 {
@@ -10,7 +6,44 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-          
+            //find "land"
+            string[] ArrayA = Array.FindAll(Names, element => element.EndsWith("land",
+                      StringComparison.Ordinal));
+            string[] ArrayB;
+            //find "stan"
+            string[] ArrayC = Array.FindAll(Names, element => element.EndsWith("stan",
+                      StringComparison.Ordinal));
+            string[] ArrayD;
+            // copy by operator =
+            ArrayB = ArrayA;
+            ArrayD = ArrayC;
+
+            Console.WriteLine("*** Array copy by operator = ***");
+            Console.WriteLine("===== Countrys =====");
+            for (int i = 0; i < Names.Length; i++)
+            { Console.WriteLine("Country[{0}] = {1}", i + 1, Names[i]); }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Find 'land' : " + (string.Join(", ", ArrayB)));
+            Console.WriteLine("Find 'stan' : " + (string.Join(", ", ArrayD)));
+            Console.ReadKey();
+        }
+        public static string[] Names = new string[]
+        {
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+     
+    "Virgin Islands, British",
+    "Virgin Islands, US",
+    "Wallis and Futuna",
+    "Western Sahara",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+        };
+
     }
 
 }
