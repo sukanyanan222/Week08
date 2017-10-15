ca0c9a3a7cf48ade7964d24e145c40eaec777683
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 12:57:20 +0700

    W08P08

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,34 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            string[] months = new string[12];
+            months[0] = "January";
+            months[1] = "February";
+            months[2] = "March";
+            months[3] = "April";
+            months[4] = "May";
+            months[5] = "June";
+            months[6] = "July";
+            months[7] = "August";
+            months[8] = "September";
+            months[9] = "October";
+            months[10] = "November";
+            months[11] = "December";
+            // a) ค้นหาชื่อเดือนแรกที่มีความยาวน้อยที่สุด
+            string value1 = Array.Find(months,
+                element => element.Length == 3);
+            // b) ค้นหาชื่อเดือนแรกที่มีความยาวมากกว่า 10 ตัวอักษร
+            string value2 = Array.Find(months,
+                element => element.Length > 10);
+            // c) ค้นหาชื่อเดือนที่มีตัวอักษรตัวแรกเหมือนเดือนมกราคม แล้วนำมาแสดงรวมกันในบรรทัดเดียว คั่นด้วย ,
+            string[] array1 = Array.FindAll(months,
+                 element => element.StartsWith("J",
+                 StringComparison.Ordinal));
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            Console.WriteLine(string.Join(",", array1));
+            Console.ReadKey();
+
         }
     }
 }
