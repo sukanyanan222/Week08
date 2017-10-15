73114cedd3672003fdbe907b865579fded11bc31
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:40:02 +0700

    W08P33

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace DictionaryDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Dictionary<string, string> country = new Dictionary<string, string>();
+            country.Add("AF", "Afghanistan");
+            country.Add("AL", "Albania");
+            country.Add("AS", "American Samoa");
+            country.Add("AD", "Andorra");
+            country.Add("AM", "Armenia");
+            country.Add("AW", "Aruba");
+            country.Add("AU", "Australia");
+            country.Add("AT", "Austria");
+            country.Add("AZ", "Azerbaijan");
+            foreach (var item in country)
+            {
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("Count of items = {0}", country.Count);
+            Console.WriteLine("\n** Direct access to value by key **");
+            country["AU"] = "AUSTRALIA";
+            Console.WriteLine("AU = " + country["AU"]);
+
+            Console.WriteLine("\n** Remove by key **");
+            country.Remove("AU");
+            foreach (var item in country)
+            {
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("Count of items = {0}", country.Count);
+            Console.ReadLine();
+
         }
     }
 }
