a28dc52d1847e3ef0f0e8afd973dde98dd39d6d9
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 12:26:51 +0700

    W08P05

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,23 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+            double[] GPS = new double[8];
+            double GPA = 0;
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                Console.Write("Enter grade on semester {0} : ", i + 1);
+                GPS[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("--------------a---------------");
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                Console.WriteLine("Grade on semester {0} : {1}", i + 1, GPS[i]);
+            }
+            Console.WriteLine("--------------b---------------");
+            for (int i = 0; i < GPS.Length; i++) { GPA += GPS[i]; }
+            Console.WriteLine("GPA is : " + GPA / GPS.Length);
+            Console.ReadKey();
+
         }
     }
 }
