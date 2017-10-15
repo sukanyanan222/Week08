0de145ec95b9d7a69c553bc8e2a5602647df0665
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 20:37:10 +0700

    W08P32

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08
+namespace DictionaryDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            dict1.Add("One", 1);
+            dict1.Add("Two", 2);
+            dict1.Add("Three", 3);
+            dict1.Add("Four", 4);
+            dict1.Add("Five", 5);
+            dict1.Add("Six", 6);
+            dict1.Add("Seven", 7);
+            dict1.Add("Eight", 8);
+            dict1.Add("Nine", 9);
+            dict1.Add("Ten", 10);
+            if (dict1.ContainsKey("One"))
+            {
+                int value = dict1["One"];
+                Console.WriteLine(value);
+            }
+            if (dict1.ContainsKey("Eleven"))
+            {
+                int value = dict1["Eleven"];
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+
         }
     }
 }
