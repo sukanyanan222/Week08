12c4a1ec6e7caac80ac27bc529b2325ce07c40b6
Sukanya <Sukanyak21130@gmail.com>
2017-10-15 19:30:24 +0700

    W08P20

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,7 +10,26 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-           
+           ArrayList list = new ArrayList();
+            Console.WriteLine("Create a list");
+            Console.WriteLine("List capacity = {0,2}", list.Capacity);
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("Add int to list : {0,2} => ", i);
+                list.Add(i);
+                Console.WriteLine("List count =  {0,3}, capacity = {1,3}  ",
+    list.Count, list.Capacity );
+            }
+            Console.WriteLine("-----------------------");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("Remove int from list : {0,2} => ", i);
+                list.Remove(i);
+                Console.WriteLine("List count =  {0,3}, capacity = {1,3}  ",
+    list.Count, list.Capacity);
+            }
+            Console.ReadLine();
+
         }
     }
 }
